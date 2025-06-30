2738fc63c2167cb24131dd1ad682d577e6cafb06
Yooondooo <kozyakovdanil04@gmail.com>
2025-06-30 19:57:22 +0300

    refactor pages commit

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,9 @@
-import os, pytest
+import os
+import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.options import Options
 
 
 def build_driver():
